4f821c36bf8237c476eac135248abe885ee4f385
zed.cole <50928706+ZedCole@users.noreply.github.com>
2021-10-06 21:16:49 +1000

    reads file, makes new dirs

--- pack_ext.py
+++ pack_ext.py
@@ -1,8 +1,11 @@
-import click, os, shutil
+import click, os, shutil, json
 
-parentDir = os.getcwd()
-tempDir = "ext_temp"
-path = os.path.join(parentDir,tempDir)
+from click.utils import echo
+
+PARENTDIRECTORY = os.getcwd()
+TEMPDIRECTORY  = os.path.join(PARENTDIRECTORY,"ext_temp")
+CONFIGFILE = 'ex_pack_list.txt'
+FILEARRAY = []
 
 @click.command()
 @click.option('--overwrite', '-o', is_flag=True, help="Will overwrite if zip file for current version already exists.")
@@ -13,10 +16,10 @@ def package(overwrite):
     if overwrite:
         click.echo("Overwriting...")
     click.echo("Temporary")
-    createTempDir()
-    copyFileItem("test.txt")
-    os.chdir(path)
-    archive()
+    # copyFileItem("test.txt")
+    # os.chdir(TEMPDIRECTORY)
+    # archive()
+    copyFileTree()
 
 """
     Create function to read a ext_pack.json file
@@ -24,22 +27,47 @@ def package(overwrite):
     Loop through files needed
     Delete ext_temp directory when done
 """
+def copyFileTree():
+    createTempDir()
+    f = open(CONFIGFILE,'r')
+    currentDir = None
+    for line in f:
+        if line.startswith('d:'):
+            if line.startswith('d:base') is False:
+                remNewLine = line.strip()
+                dirName = remNewLine.split(':')
+                currentDir = dirName
+                newDir = os.path.join(TEMPDIRECTORY,dirName[1])
+                os.mkdir(newDir)
+            else:
+                currentDir = "base"
+        else:
+            remNewLine = line.strip()
+            fileName = remNewLine.split(':')
+            if currentDir == "base":
+                print(fileName)
+                copyFileItem(fileName[1])
+            else:
+                # newFilePath = os.path.join(currentDir,fileName[1])
+                # click.echo(newFilePath)
+                copyFileItem(currentDir,fileName[1]) #something wrong here - no like directory
+    f.close()
 
 def createTempDir():
-    os.mkdir(path)
+    os.mkdir(TEMPDIRECTORY)
 
 def removeTempDir():
-    shutil.rmtree(path)
+    shutil.rmtree(TEMPDIRECTORY)
 
 def copyFileItem(fileName):
-    srcFile = os.path.join(parentDir, fileName)
-    destFile = os.path.join(path, fileName)
+    srcFile = os.path.join(PARENTDIRECTORY, fileName)
+    destFile = os.path.join(TEMPDIRECTORY, fileName)
     shutil.copyfile(srcFile, destFile)
 
 def archive(archiveName):
-    outputFolder = os.path.join(parentDir,'output')
+    outputFolder = os.path.join(PARENTDIRECTORY,'output')
     outputFile = os.path.join(outputFolder,archiveName)
-    shutil.make_archive(outputFile, 'zip', path)
+    shutil.make_archive(outputFile, 'zip', TEMPDIRECTORY)
 
 if __name__ == '__main__':
     package()
