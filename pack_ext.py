0248afeb668351117aef88c262c0a461a0c1407a
zed.cole <50928706+ZedCole@users.noreply.github.com>
2021-10-09 17:19:12 +1000

    many changes, havn't posted whoops

--- pack_ext.py
+++ pack_ext.py
@@ -4,22 +4,40 @@ from click.utils import echo
 
 PARENTDIRECTORY = os.getcwd()
 TEMPDIRECTORY  = os.path.join(PARENTDIRECTORY,"ext_temp")
-CONFIGFILE = 'ex_pack_list.txt'
-FILEARRAY = []
+CONFIGFILE = '.packignore'
 
 @click.command()
 @click.option('--overwrite', '-o', is_flag=True, help="Will overwrite if zip file for current version already exists.")
+@click.option('--conf', '-c', is_flag=True, help="Pass a config file.")
 
+# New direction to take script:
+# store files & directories in an array (check how to search directory recursively)
+# check current file against array
+# if it exists in the array ignore the current one
+# if id does not exist in the array copy the file
+
+# build a function to check the current line for "\/." and count that as a file (edge case "files that dont have extensions")
+# read each division and  enter into a set array (dir,file) 
+# do the appropriate
 
 # Rewrite this with final code
-def package(overwrite):
+def package(overwrite,conf):
     if overwrite:
         click.echo("Overwriting...")
     click.echo("Temporary")
+    if conf:
+        file = readconfig(conf) #this is not done
+    else:
+        file = readconfig(CONFIGFILE)
+    for line in file:
+        print("Line " + ": " + line.strip())
+    checkDir()
     # copyFileItem("test.txt")
     # os.chdir(TEMPDIRECTORY)
     # archive()
-    copyFileTree()
+    # copyFileTree()
+    # createTempDir()
+    # removeTempDir()
 
 """
     Create function to read a ext_pack.json file
@@ -27,31 +45,26 @@ def package(overwrite):
     Loop through files needed
     Delete ext_temp directory when done
 """
-def copyFileTree():
-    createTempDir()
-    f = open(CONFIGFILE,'r')
-    currentDir = None
-    for line in f:
-        if line.startswith('d:'):
-            if line.startswith('d:base') is False:
-                remNewLine = line.strip()
-                dirName = remNewLine.split(':')
-                currentDir = dirName
-                newDir = os.path.join(TEMPDIRECTORY,dirName[1])
-                os.mkdir(newDir)
-            else:
-                currentDir = "base"
-        else:
-            remNewLine = line.strip()
-            fileName = remNewLine.split(':')
-            if currentDir == "base":
-                print(fileName)
-                copyFileItem(fileName[1])
-            else:
-                # newFilePath = os.path.join(currentDir,fileName[1])
-                # click.echo(newFilePath)
-                copyFileItem(currentDir,fileName[1]) #something wrong here - no like directory
-    f.close()
+
+def readconfig(config):
+    read = os.path.join(PARENTDIRECTORY, config)
+    file = open(read, 'r')
+    return file
+
+# use tuples? [(path, directory), (path, directory)]
+# only remove directory if it exists under path
+
+excludeDir = ["notme","ignorethisfolder"]
+excludeFile = ["pleaseignoreme.txt",".packignore"]
+# run through directories & sub-directories
+def checkDir():
+    for root, dirs, files in os.walk(os.getcwd(), topdown=True):
+        dirs[:] = [d for d in dirs if d not in excludeDir]
+        files[:] = [f for f in files if f not in excludeFile]
+        print(root)
+        print(dirs)
+        print(files)
+        print("-----------")
 
 def createTempDir():
     os.mkdir(TEMPDIRECTORY)
