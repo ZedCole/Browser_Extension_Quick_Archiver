85a62f5b914d5f50eb1d9894f2d6227feedf42e7
zed.cole <50928706+ZedCole@users.noreply.github.com>
2021-10-16 16:32:15 +1000

    Remove ignored files & dirs from walk

--- pack_ext.py
+++ pack_ext.py
@@ -36,7 +36,6 @@ def readconfig(config):
 def readPath(root):
     lenPar = len(str(PARENTDIRECTORY))
     return root[lenPar + 1:]
-    # print(root[lenPar + 1:])
 
 # run through directories & sub-directories
 def checkDir():
@@ -44,13 +43,39 @@ def checkDir():
         dirs[:] = [d for d in dirs if d not in IGNOREDIRS]      # use these to remove other files & directories
         files[:] = [f for f in files if f not in IGNOREFILES]   # copy what is left, just loop the array 
         path = readPath(root)
-        debugPrint(path,dirs,files)   
+        if path == "":
+            path = None
+        debugPrint(path,dirs,files)
+        print(removeIgnoredFiles(path,files))
+        print(removeIgnoredDirectory(path,dirs))
+        print("---------------\n")
+
+def removeIgnoredDirectory(path,directory_list):
+    for item in EXCLUDED:
+        if item[0] == path and item[1] is not None:
+            directory_list.remove(item[1])
+    
+    return directory_list
+
+def removeIgnoredFiles(path,file_list):
+    # Check for *.extension expressions
+    """Todo: add functionality for ignoring extensions i.e. *.log"""
+    for item in EXCLUDED:
+        if item[0] is None and item[2] is not None:
+            try:
+                file_list.remove(item[2])
+            except:
+                pass
+        elif item[0] == path and item[2] is not None:
+            print(item[2])
+            file_list.remove(item[2])
+
+    return file_list
 
 def debugPrint(path,directory_list,file_list):
-    print("Current Directory: ", "Base Directory" if path == "" else path)
+    print("Current Directory: ", "Base Directory" if path is None else path)
     print("Sub Directories: " + str(directory_list))
     print("Files: " + str(file_list))
-    print("---------------\n")
 
 def createTempDir():
     os.mkdir(TEMPDIRECTORY)
